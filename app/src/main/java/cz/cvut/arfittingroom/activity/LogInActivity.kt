7a0e4b0a1ffe75f9c9db2b849b646aff1126d21a
veronikaovsyannikova <ovsyaver@fel.cvut.fel>
2024-04-30 00:33:16 +0200

    login works

--- app/src/main/java/cz/cvut/arfittingroom/activity/LogInActivity.kt
+++ app/src/main/java/cz/cvut/arfittingroom/activity/LogInActivity.kt
@@ -1,25 +1,36 @@
 package cz.cvut.arfittingroom.activity
 
+import android.R.attr.password
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 import cz.cvut.arfittingroom.databinding.ActivityLoginBinding
-import cz.cvut.arfittingroom.databinding.ActivityMainBinding
-import cz.cvut.arfittingroom.databinding.ActivitySignupBinding
 
-class LogInActivity: AppCompatActivity() {
+
+class LogInActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-
+    private lateinit var auth: FirebaseAuth
     private lateinit var fileStore: FirebaseFirestore
     private lateinit var usernameInput: EditText
     private lateinit var pwdInput: EditText
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         fileStore = FirebaseFirestore.getInstance()
+        auth = Firebase.auth
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
 
@@ -28,6 +39,11 @@ class LogInActivity: AppCompatActivity() {
         usernameInput = binding.username
         pwdInput = binding.pwd
 
+        binding.buttonLogIn.setOnClickListener {
+            if(validateInput(usernameInput.text.toString().trim(), pwdInput.text.toString().trim())) {
+                login(usernameInput.text.toString().trim(), pwdInput.text.toString().trim())
+            }
+        }
 
         binding.buttonSingUp.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
@@ -36,6 +52,58 @@ class LogInActivity: AppCompatActivity() {
         binding.buttonReturn.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+    }
+
+
+    private fun validateInput(username: String, pwd: String): Boolean {
+        var isValid = true
+
+        usernameInput.error = null
+        pwdInput.error = null
+
+        if (username.isEmpty()) {
+            usernameInput.error = "Username is required"
+            isValid = false
+        }
+
+
+        if (pwd.isEmpty()) {
+            pwdInput.error = "Password is required"
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    private fun login(username: String, pwd: String) {
+        auth.signInWithEmailAndPassword("$username@glamartist.com", pwd)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.println(Log.INFO, null, "User $username logged in")
+                    Toast.makeText( baseContext, "Logged in Successfully", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, ShowRoomActivity::class.java))
+                } else {
+
+                    Log.println(Log.ERROR, null, "createUserWithEmail:failure ${task.exception}")
+                    val errorMsg = when (task.exception) {
+                        is FirebaseAuthInvalidCredentialsException -> {
+                            "Invalid password or username"
+                        }
+
+                        is FirebaseNetworkException -> {
+                            "Network error"
+                        }
+                        else -> {
+                            "Something went wrong, please, try again"
+                        }
+                    }
+                    Toast.makeText(
+                        baseContext,
+                        errorMsg,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 
 }
